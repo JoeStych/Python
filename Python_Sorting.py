f07694f7734db261a173190533c32c4e92597c3b
JoeStych <98861933+JoeStych@users.noreply.github.com>
2023-09-14 11:23:28 -0500

    Add files via upload

---
+++ Python_Sorting.py
@@ -0,0 +1,82 @@
+# Sorting algorithms for python
+
+import random
+
+def mergeSort(myList):
+    if len(myList) > 1:
+        mid = len(myList) // 2
+        left = myList[:mid]
+        right = myList[mid:]
+
+        # Recursive call on each half
+        mergeSort(left)
+        mergeSort(right)
+
+        # Two iterators for traversing the two halves
+        i = 0
+        j = 0
+        
+        # Iterator for the main list
+        k = 0
+        
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]:
+              # The value from the left half has been used
+              myList[k] = left[i]
+              # Move the iterator forward
+              i += 1
+            else:
+                myList[k] = right[j]
+                j += 1
+            # Move to the next slot
+            k += 1
+
+        # For all the remaining values
+        while i < len(left):
+            myList[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            myList[k]=right[j]
+            j += 1
+            k += 1
+
+
+def partition(l, r, nums):
+    # Last element will be the pivot and the first element the pointer
+    pivot, ptr = nums[r], l
+    for i in range(l, r):
+        if nums[i] <= pivot:
+            # Swapping values smaller than the pivot to the front
+            nums[i], nums[ptr] = nums[ptr], nums[i]
+            ptr += 1
+    # Finally swappping the last element with the pointer indexed number
+    nums[ptr], nums[r] = nums[r], nums[ptr]
+    return ptr
+ 
+# With quicksort() function, we will be utilizing the above code to obtain the pointer
+# at which the left values are all smaller than the number at pointer index and vice versa
+# for the right values.
+ 
+ 
+def quickSort(l, r, nums): #first, last, [list]
+    if len(nums) == 1:  # Terminating Condition for recursion. VERY IMPORTANT!
+        return nums
+    if l < r:
+        pi = partition(l, r, nums)
+        quickSort(l, pi-1, nums)  # Recursively sorting the left values
+        quickSort(pi+1, r, nums)  # Recursively sorting the right values
+    return nums
+            
+
+complexity = 10000
+capacity = 1000
+x = []
+
+for i in range(1000):
+    x.append(random.randint(0, complexity))
+
+quickSort(0, 999, x)
+
+print(x)
